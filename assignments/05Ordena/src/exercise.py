7117f59bfcda6c7243f80b4dd962978724e16543
PabloSoberanis <89001142+PabloSoberanis@users.noreply.github.com>
2021-08-25 22:18:10 -0500

    Update exercise.py

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -1,24 +1,24 @@
 def main():
     # Escribe el código adecuado para completar el programa
-    print("Ingresa el primer número")
+    print("Ingresa el primer número:")
     x = int(input())
-    print("Ingresa el segundo número")
+    print("Ingresa el segundo número:")
     y = int(input())
     print("Ingresa el tercer número")
     z = int(input())
 
     if(x<y and y<z):
-        print(x,"-",y,"-",z)
+        print("\n", x,"\n",y,"\n",z)
     elif(y<x and x<z):
-        print(y,"-",x,"-",z)
+        print("\n",y,"\n",x,"\n",z)
     elif(z<x and x<y):
-        print(z,"-",x,"-",y)
-    elif(z<y and y<x):
-        print(z,"-",y,"-",x) 
+        print("\n",z,"\n",x,"\n",y)
+    elif(z<y and y<x): 
+        print("\n",z,"\n",y,"\n",x) 
     elif(x<z and z<y):
-        print(x,"-",z,"-",y)
+        print("\n",x,"\n",z,"\n",y)
     elif(y<z and z<x):
-        print(y,"-",z,"-",x)
+        print("\n",y,"\n",z,"\n",x)
     pass
 
 
