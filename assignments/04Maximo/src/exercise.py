db4211c76751e8834c6d4c8ff230606ebd35ef39
PabloSoberanis <89001142+PabloSoberanis@users.noreply.github.com>
2021-08-25 20:28:22 -0500

    Update exercise.py

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
+    x = int(input("Ingresa el primer número: "))
+    y = int(input("Ingresa el segundo número: "))
+    z = int(input("Ingresa el tercer número: "))
+    if( y < x > z):
+        print(x)
+    if( x < y > z):
+        print(y)
+    if( y < z > x):
+        print(z)
     pass
 
 
